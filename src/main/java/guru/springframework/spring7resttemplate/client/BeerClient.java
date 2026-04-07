8f391a2faa426313596d691cc1bfe4d974b8d287
ROG4113 <as0009916@gmail.com>
2026-04-07 22:31:55 +0530

    adding pojo

---
+++ src/main/java/guru/springframework/spring7resttemplate/client/BeerClient.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring7resttemplate.client;
+
+import org.springframework.data.domain.Page;
+
+import guru.springframework.spring7resttemplate.model.BeerDTO;
+
+public interface BeerClient {
+    Page<BeerDTO> listBeers();
+}

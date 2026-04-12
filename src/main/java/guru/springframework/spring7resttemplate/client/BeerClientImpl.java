f03a7c7af98a2781a26977856b1686699edf9b24
ROG4113 <as0009916@gmail.com>
2026-04-12 19:40:08 +0530

    adding get list

--- src/main/java/guru/springframework/spring7resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring7resttemplate/client/BeerClientImpl.java
@@ -1,14 +1,30 @@
 package guru.springframework.spring7resttemplate.client;
 
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import guru.springframework.spring7resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
 
     @Override
     public Page<BeerDTO> listBeers() {
-        // TODO Auto-generated method stub
+        
+        RestTemplate restTemplate=restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse=
+            restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+        System.out.println(stringResponse.getBody());
+
         return null;
     }
 
---
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring7resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeerClientImplTest {
+    @Autowired
+    BeerClientImpl beerClient;
+
+    @Test
+    void testListBeers() {
+        beerClient.listBeers();
+    }
+}
